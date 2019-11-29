d0e5d37b1cff27dc48681be271f38660e0880f2d
Ivan Tihonov <navi2853@gmail.com>
2019-11-29 17:58:49 +0300

    add rope project

---
+++ Research.Rope/Program.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Research.Rope
+{
+    public class Benchmark
+    {
+        string source = "Buffer";
+
+        [Benchmark]
+        public void SpanCopy()
+        {
+            var buffer = new char[16];
+            source.ToCharArray().AsSpan().CopyTo(buffer);
+        }
+
+        [Benchmark]
+        public void ArrayCopy()
+        {
+            var buffer = new char[16];
+            Array.Copy(source.ToCharArray(), 0, buffer, 0, source.Length);
+        }
+
+        [Benchmark]
+        public void BlockCopy()
+        {
+            var buffer = new char[16];
+            Buffer.BlockCopy(source.ToCharArray(), 0, buffer, 0, sizeof(char) * source.Length);
+        }
+
+        class Program
+        {
+            static void Main(string[] args) => BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
---
+++ Research.Rope/Rope.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Research.Rope
+{
+    public class Rope
+    {
+        private readonly Node _root;
+
+        private const int NodeCapacity = 256;
+
+        public int Length => _root._length;
+
+        public Rope() : this(new Node())
+        { }
+
+        private Rope(Node root) => _root = root;
+
+        public void Insert(int index, string text) => Insert(index, text.AsSpan());
+
+        public void Insert(int index, string text, int count) => Insert(index, text.AsSpan(0, count));
+
+        public void Insert(int index, ReadOnlySpan<char> text)
+        {
+            if (index > _root._length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        private static Node Insert(Node node, int index, ReadOnlySpan<char> text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char this[int index] => GetChar(_root, index);
+
+        private static char GetChar(Node node, int index)
+        {
+            if (index >= node._length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (node.IsLeaf)
+            {
+                return node._content[index];
+            }
+
+            return index < node._left._length ? GetChar(node._left, index) : GetChar(node._right, index - node._left._length - 1);
+        }
+
+        private static Node CreateLeaf() => CreateNode(true);
+
+        private static Node CreateNode(bool leaf = false)
+        {
+            var n = new Node();
+
+            if (leaf)
+            {
+                n._content = new char[NodeCapacity];
+            }
+
+            return n;
+        }
+
+        class Node
+        {
+            public Node _left;
+
+            public Node _right;
+
+            public int _length;
+
+            public char[] _content;
+
+            public bool IsLeaf => _content != null;
+        }
+    }
+}
